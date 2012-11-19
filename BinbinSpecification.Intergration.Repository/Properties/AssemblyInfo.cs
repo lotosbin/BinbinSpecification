04152b49a22fed01115b60aa7cef27f649513dc9
lotosbin <lotosbin@gmail.com>
2012-11-19 18:43:28 +0800

    add SpecificationIntergrationRepository

--- BinbinSpecification.Intergration.Repository/Properties/AssemblyInfo.cs
+++ BinbinSpecification.Intergration.Repository/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("BinbinSpecification")]
-[assembly: AssemblyDescription("BinbinSpecification")]
-[assembly: AssemblyConfiguration("BinbinSpecification")]
-[assembly: AssemblyCompany("lotosbin@gmail.com")]
-[assembly: AssemblyProduct("BinbinSpecification")]
+[assembly: AssemblyTitle("BinbinSpecification.Intergration.Repository")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BinbinSpecification.Intergration.Repository")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -17,12 +17,10 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("f6d37db5-bceb-421e-80c5-57e01e1dcc75")]
+[assembly: Guid("2cf34b2d-380a-4b55-a272-bc6998dba80b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BinbinSpecification.Intergration.Repository/SpecificationRepositoryExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BinbinRepository;
+
+namespace BinbinSpecification.Intergration.Repository
+{
+    public static class SpecificationRepositoryExtension
+    {
+        public static IEnumerable<TEntity> Find<TEntity, TKey>(this IRepository<TEntity, TKey> repository, ISpecification specification) where TEntity : class
+        {
+            return repository.Find(t => true).ToList().Where(specification.IsSatisfiedBy);
+        }
+    }
+}
